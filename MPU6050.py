791aa72508c5c1236ceb5d4a1da7bf352f5f35af
Stephen Mwangi <62056954+Stevemwa@users.noreply.github.com>
2023-11-07 11:49:41 +0300

    Add files via upload

--- MPU6050.py
+++ MPU6050.py
@@ -8,6 +8,15 @@ import smbus2
 from time import sleep
 
 class IMU:
+    #MPU_6050 registers
+    ACCEL_XOUT0 = 0x3B
+    ACCEL_YOUT0 = 0x3D
+    ACCEL_ZOUT0 = 0x3F
+    GYRO_XOUT0 = 0x43
+    GYRO_YOUT0 = 0x45
+    GYRO_ZOUT0 = 0x47
+
+
     def __init__(self, bus=1, address=0x68):
         self.bus = smbus2.SMBus(bus)
         self.address = address
@@ -17,28 +26,47 @@ class IMU:
         self.bus.write_byte_data(self.address, 0x19, 0)  # Set sample rate divider to 0 for maximum sensitivity
         self.bus.write_byte_data(self.address, 0x1A, 0x03)  # Set DLPF (Digital Low Pass Filter) to 42Hz
         self.bus.write_byte_data(self.address, 0x1B, 0x00)  # Set the gyro range to +/- 250 degrees/sec
-        self.bus.write_byte_data(self.address, 0x1C, 0x00)  # Set the accelerometer range to +/- 2g
+        self.bus.write_byte_data(self.address, 0x1C, 0x10)  # Set the accelerometer range to +/- 8g
+        self.bus.write_byte_data(self.address, 0x1B, 0x10)  # Set  Gyro range to 1000 Degrees
 
-    def read_data(self):
-        data = self.bus.read_i2c_block_data(self.address, 0x3B, 14)
+    def read_data(self,register):
+        data_high = self.bus.read_byte_data(self.address, register)
+        data_low = self.bus.read_byte_data(self.address, register + 1)
+        data =(data_high << 8)+ data_low
+        if (data >= 0x8000):
+            return -((65535 - data) + 1)
+        else:
+            return data
+
+    def get_acceleration(self):
+         x = self.read_i2c_word(self.ACCEL_XOUT0)
+         y = self.read_i2c_word(self.ACCEL_YOUT0)
+         z = self.read_i2c_word(self.ACCEL_ZOUT0)
+         x=float(x)/4096.0
+         y=float(y)/4096.0
+         z=float(z)/4096.0
+         return x,y,z
+    
+    def get_gyro(self):
+        x = self.read_i2c_word(self.GYRO_XOUT0)
+        y = self.read_i2c_word(self.GYRO_YOUT0)
+        z = self.read_i2c_word(self.GYRO_ZOUT0)
+        x=float(x)/1000.0
+        y=float(y)/1000.0
+        z=float(z)/1000.0
+        return x,y,z
+    
+    def get_all_data(self):
+        a = self.get_accel_data()
+        g = self.get_gyro_data()
+        return [a,g]
+
+
+
         
-        # Combine the high and low bytes to get the raw data
-        accelerometer_data = [
-            (data[0] << 8 | data[1]),
-            (data[2] << 8 | data[3]),
-            (data[4] << 8 | data[5])
-        ]
-        gyro_data = [
-            (data[8] << 8 | data[9]),
-            (data[10] << 8 | data[11]),
-            (data[12] << 8 | data[13])
-        ]
-        
-        # Convert to actual values (acceleration in g, angular velocity in degrees/s)
-        acceleration = [val / 16384.0 for val in accelerometer_data]
-        gyro = [val / 131.0 for val in gyro_data]
-        
-        return acceleration, gyro
+       
+    
+
 
     
     
